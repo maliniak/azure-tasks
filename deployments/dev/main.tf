0c77573d630f66d8bbbd87f8c6c3ce4da7f28de3
Michal Malinowski <michal.malinowski@falck.com>
2025-02-27 10:32:38 +0100

    fmt

--- deployments/dev/main.tf
+++ deployments/dev/main.tf
@@ -1,12 +1,12 @@
 module "azure_app" {
-    source = "../../applications/web_app"
-    environment = "dev"
-    mysql_sku_name       = "B_Standard_B1s"
-    mysql_storage_size   = 20
-    backup_retention_days = 7
-    app_service_sku      = "B1"
-    docker_image         = "my-nodejs-app:latest"
-    docker_registry_url  = "https://index.docker.io/v1/"
-    vnet_address_space = "10.1.0.0/16"
-    subnet_address_prefix = "10.1.1.0/24"
+  source                = "../../applications/web_app"
+  environment           = "dev"
+  mysql_sku_name        = "B_Standard_B1s"
+  mysql_storage_size    = 20
+  backup_retention_days = 7
+  app_service_sku       = "B1"
+  docker_image          = "my-nodejs-app:latest"
+  docker_registry_url   = "https://index.docker.io/v1/"
+  vnet_address_space    = "10.1.0.0/16"
+  subnet_address_prefix = "10.1.1.0/24"
 }
--- deployments/production/providers.tf
+++ deployments/production/providers.tf
@@ -4,9 +4,9 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "tf_backend"
-    storage_account_name  = "terraformstate01azuretks"
-    container_name        = "tfbackend-prod"
-    key                   = "terraform.tfstate"
+    resource_group_name  = "tf_backend"
+    storage_account_name = "terraformstate01azuretks"
+    container_name       = "tfbackend-prod"
+    key                  = "terraform.tfstate"
   }
 }
