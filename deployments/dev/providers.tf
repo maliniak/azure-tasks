814e54dcdf2df6a85fce292590dfe6be2f5b65ec
Michal Malinowski <michal.malinowski@falck.com>
2025-02-28 05:09:24 +0100

    shared env

--- deployments/dev/providers.tf
+++ deployments/dev/providers.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
-  subscription_id = ${{ secrets.AZURE_SUBSCRIPTION_ID }}
-  client_id       = ${{ secrets.AZURE_CLIENT_ID }}
-  client_secret   = ${{ secrets.AZURE_CLIENT_SECRET }}
-  tenant_id       = ${{ secrets.AZURE_TENANT_ID }}
+  #subscription_id = "${{ secrets.AZURE_SUBSCRIPTION_ID }}"
+  #client_id       = "${{ secrets.AZURE_CLIENT_ID }}"
+  #client_secret   = "${{ secrets.AZURE_CLIENT_SECRET }}"
+  #tenant_id       = "${{ secrets.AZURE_TENANT_ID }}"
 }
 
 terraform {
