3efe3e58c51e95c2aef12f247171b954f5cbf56e
Michal Malinowski <michal.malinowski@falck.com>
2025-02-28 06:29:42 +0100

    oidc

--- deployments/dev/providers.tf
+++ deployments/dev/providers.tf
@@ -1,12 +1,18 @@
 provider "azurerm" {
   features {}
-  #subscription_id = "${{ secrets.AZURE_SUBSCRIPTION_ID }}"
-  #client_id       = "${{ secrets.AZURE_CLIENT_ID }}"
-  #client_secret   = "${{ secrets.AZURE_CLIENT_SECRET }}"
-  #tenant_id       = "${{ secrets.AZURE_TENANT_ID }}"
 }
 
 terraform {
+    required_version = ">=1.9"
+
+    required_providers {
+      azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "4.21.0"
+      }
+    }
+
+
   backend "azurerm" {
     resource_group_name  = "tf_backend"
     storage_account_name = "terraformstate01azuretks"
